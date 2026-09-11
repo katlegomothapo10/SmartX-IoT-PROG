3abb5e9c385cda967f5b272c49b4d743c61e493d
emeris <emeris@LabVM2302769>
2026-09-11 13:05:22 +0200

    Fixed Telemetry Packet Class

---
+++ SmartX.Client/Models/TelemetryPacket.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartX.Client.Models;
+
+public class TelemetryPacket<T>
+{
+    [JsonPropertyName("deviceMac")]
+    public string DeviceMac { get; set; } = string.Empty;
+
+    [JsonPropertyName("timestamp")]
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    [JsonPropertyName("dataType")]
+    public string DataType { get; set; } = typeof(T).Name;
+
+    [JsonPropertyName("sensorType")]
+    public SensorType SensorType { get; set; }
+
+    [JsonPropertyName("value")]
+    public T Value { get; set; } = default!;
+
+    [JsonPropertyName("metadata")]
+    public Dictionary<string, object> Metadata { get; set; } = new();
+
+    [JsonPropertyName("qualityScore")]
+    public double QualityScore { get; set; } = 1.0;
+}
+
+public class TelemetryRecord
+{
+    [JsonPropertyName("deviceMac")]
+    public string DeviceMac { get; set; } = string.Empty;
+
+    [JsonPropertyName("timestamp")]
+    public DateTime Timestamp { get; set; }
+
+    [JsonPropertyName("dataType")]
+    public string DataType { get; set; } = string.Empty;
+
+    [JsonPropertyName("sensorType")]
+    public SensorType SensorType { get; set; }
+
+    [JsonPropertyName("value")]
+    public object? Value { get; set; }
+
+    [JsonPropertyName("metadata")]
+    public Dictionary<string, object> Metadata { get; set; } = new();
+}
