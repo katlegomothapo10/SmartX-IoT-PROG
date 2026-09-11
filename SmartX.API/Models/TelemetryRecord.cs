45fea2abd9de285933c0514da8f3b915a64f0fc8
emeris <emeris@LabVM2302769>
2026-09-11 13:13:39 +0200

    Added Telemetry Record File to Fix The Errors

---
+++ SmartX.API/Models/TelemetryRecord.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartX.API.Models;
+
+public class TelemetryRecord
+{
+    [JsonPropertyName("deviceMac")]
+    public string DeviceMac { get; set; } = string.Empty;
+
+    [JsonPropertyName("timestamp")]
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    [JsonPropertyName("dataType")]
+    public string DataType { get; set; } = string.Empty;
+
+    [JsonPropertyName("sensorType")]
+    public SensorType SensorType { get; set; }
+
+    [JsonPropertyName("value")]
+    public object? Value { get; set; }
+
+    [JsonPropertyName("metadata")]
+    public Dictionary<string, object> Metadata { get; set; } = new();
+}
